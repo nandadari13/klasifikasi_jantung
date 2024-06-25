12483c6c108db34ce1b08a1d4efd3721ac593432
nandadari13 <149654491+nandadari13@users.noreply.github.com>
2024-06-25 12:54:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Judul Aplikasi
 st.title("Aplikasi Prediksi Penyakit Jantung")
@@ -11,19 +10,19 @@ st.title("Aplikasi Prediksi Penyakit Jantung")
 # Fungsi untuk mengumpulkan input pengguna
 st.sidebar.header("Input Parameter")
 def user_input_features():
-    age = st.sidebar.number_input("Age", 20, 100, 50)
-    sex = st.sidebar.selectbox("Sex", (0, 1))
-    cp = st.sidebar.selectbox("Chest Pain Type (0-3)", (0, 1, 2, 3))
-    trestbps = st.sidebar.number_input("Resting Blood Pressure", 80, 200, 120)
-    chol = st.sidebar.number_input("Serum Cholestoral (mg/dl)", 100, 400, 200)
-    fbs = st.sidebar.selectbox("Fasting Blood Sugar > 120 mg/dl", (0, 1))
-    restecg = st.sidebar.selectbox("Resting Electrocardiographic Results (0-2)", (0, 1, 2))
-    thalach = st.sidebar.number_input("Maximum Heart Rate Achieved", 70, 210, 150)
-    exang = st.sidebar.selectbox("Exercise Induced Angina", (0, 1))
-    oldpeak = st.sidebar.number_input("ST Depression Induced by Exercise", 0.0, 6.0, 1.0)
-    slope = st.sidebar.selectbox("Slope of the Peak Exercise ST Segment", (0, 1, 2))
-    ca = st.sidebar.selectbox("Number of Major Vessels (0-4)", (0, 1, 2, 3, 4))
-    thal = st.sidebar.selectbox("Thalassemia (0-3)", (0, 1, 2, 3))
+    age = st.sidebar.number_input("Age (20-100)", 20, 100, 50)
+    sex = st.sidebar.selectbox("Sex (0 = Female, 1 = Male)", (0, 1))
+    cp = st.sidebar.selectbox("Chest Pain Type (0 = Typical Angina, 1 = Atypical Angina, 2 = Non-Anginal Pain, 3 = Asymptomatic)", (0, 1, 2, 3))
+    trestbps = st.sidebar.number_input("Resting Blood Pressure (80-200 mm Hg)", 80, 200, 120)
+    chol = st.sidebar.number_input("Serum Cholestoral (100-400 mg/dl)", 100, 400, 200)
+    fbs = st.sidebar.selectbox("Fasting Blood Sugar > 120 mg/dl (0 = False, 1 = True)", (0, 1))
+    restecg = st.sidebar.selectbox("Resting Electrocardiographic Results (0 = Normal, 1 = Having ST-T Wave Abnormality, 2 = Showing Probable or Definite Left Ventricular Hypertrophy)", (0, 1, 2))
+    thalach = st.sidebar.number_input("Maximum Heart Rate Achieved (70-210 bpm)", 70, 210, 150)
+    exang = st.sidebar.selectbox("Exercise Induced Angina (0 = No, 1 = Yes)", (0, 1))
+    oldpeak = st.sidebar.number_input("ST Depression Induced by Exercise (0.0-6.0)", 0.0, 6.0, 1.0)
+    slope = st.sidebar.selectbox("Slope of the Peak Exercise ST Segment (0 = Upsloping, 1 = Flat, 2 = Downsloping)", (0, 1, 2))
+    ca = st.sidebar.selectbox("Number of Major Vessels (0-4) Colored by Fluoroscopy", (0, 1, 2, 3, 4))
+    thal = st.sidebar.selectbox("Thalassemia (1 = Normal, 2 = Fixed Defect, 3 = Reversable Defect)", (0, 1, 2, 3))
     
     data = {
         'age': age,
@@ -77,22 +76,8 @@ knn_classifier = KNeighborsClassifier(n_neighbors=k)
 # Melatih model
 knn_classifier.fit(X_train, y_train)
 
-# Memprediksi label
-y_pred = knn_classifier.predict(X_test)
-
-# Evaluasi model
-accuracy = accuracy_score(y_test, y_pred)
-
 # Prediksi menggunakan input pengguna
 user_prediction = knn_classifier.predict(df_scaled)
-
-# Tampilkan hasil evaluasi
-# st.subheader('Model Evaluation')
-# st.write(f"Akurasi: {accuracy:.2f}")
-# st.write("Classification Report:")
-# st.text(classification_report(y_test, y_pred))
-# st.write("Confusion Matrix:")
-# st.write(confusion_matrix(y_test, y_pred))
 
 # Tampilkan hasil prediksi untuk input pengguna
 st.subheader('Hasil Prediksi')
