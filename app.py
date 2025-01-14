138816f985b5a3f8db4f4710601e8b040b655459
nandadari13 <149654491+nandadari13@users.noreply.github.com>
2025-01-14 21:07:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ user_prediction = knn_classifier.predict(df_scaled)
 
 # Tampilkan hasil prediksi untuk input pengguna
 st.subheader('Hasil Prediksi')
-st.write('Penyakit Jantung' if user_prediction[0] == 0 else 'Tidak Ada Penyakit Jantung')
+st.write('Penyakit Jantung' if user_prediction[0] == 1 else 'Tidak Ada Penyakit Jantung')
